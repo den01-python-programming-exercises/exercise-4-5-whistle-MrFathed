2ebcbc2d65de50199dc95d1fb616aea8af433bdb
Justin Turner <jmturner638@gmail.com>
2021-08-03 07:39:39 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -2,6 +2,12 @@ from whistle import Whistle # imports
 
 def main():
     #write your code below this line
+    duck_whistle = Whistle("Kvaak")
+    rooster_whistle = Whistle("Peef")
+
+    print(duck_whistle.sound)
+    print(rooster_whistle.sound)
+    print(duck_whistle.sound)
 
 if __name__ == '__main__':
     main()
--- src/whistle.py
+++ src/whistle.py
@@ -1,1 +1,3 @@
 class Whistle:
+    def __init__(self, whistle_sound):
+        self.sound = whistle_sound
